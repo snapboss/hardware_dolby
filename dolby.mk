36f3ff9d0b12c6681ce5c2f061eb6f5bfd562eec
Luxured <mbajrami95@gmail.com>
2025-02-15 17:02:39 +0100

    Adapt to Duchamp

--- dolby.mk
+++ dolby.mk
@@ -31,10 +31,8 @@ PRODUCT_PACKAGES += \
 BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/configs/vintf/dolby_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
-    $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml \
-    $(DOLBY_PATH)/configs/vintf/vendor.dolby.media.c2@1.0-service.xml
+    $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml
 
 # Configs
 PRODUCT_COPY_FILES += \
