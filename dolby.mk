3d60d9f619e4e64a14a4137d2df192a9ba1db8f7
snapboss <mbajrami95@gmail.com>
2025-03-26 04:41:57 +0100

    duchamp: Add missing dolby media c2

--- dolby.mk
+++ dolby.mk
@@ -32,6 +32,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
 # HIDL
 DEVICE_MANIFEST_FILE += \
+    $(DOLBY_PATH)/configs/vintf/vendor.dolby.media.c2@1.0-service.xml \
     $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml
 
 # Configs
