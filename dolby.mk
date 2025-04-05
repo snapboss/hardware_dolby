beb6ad6d56508516ac461c30d47267e290774891
Abhay Singh Gill <abhaygill017@gmail.com>
2025-04-05 18:52:00 +0000

    reapply! dolby: Build codec2 required packages

--- dolby.mk
+++ dolby.mk
@@ -28,6 +28,12 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 DEVICE_MANIFEST_FILE += \
     $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml
 
+# Build codec2 packages
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libcodec2_soft_common.vendor
+
 # Configs
 PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/configs/dax/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
